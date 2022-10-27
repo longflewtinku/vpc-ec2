0ac960333b8f76205ac3a4536d8a0402c1081a0a
tinku <laxitinku7788@gmail.com>
2022-10-27 12:47:48 +0530

    add-4

--- awscli.sh
+++ awscli.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-aws ec2 create-vpc \
-    --cidr-block "10.0.0.0/16" \
-    --region "us-west-2" \
-    --tag-specification "ResourceType=vpc, Tags=[{Key=name,Value=my-vpc}]" \
-    --query Vpc.VpcId
+aws ec2 run-instances \
+    --image-id "ami-024c319d5d14b463e" \
+    --tag-specification "ResourceType=instances,Tags=[{Key=Name,Value=public-instance-1}]" \
+    --count "1" \
+    --key-name "jenkina key" \
+    --region "ap-south-1" \
+    --security-group-ids "sg-0b97a5f1b8d7da3e3" \
+    --instance-type "t2.micro" \
+    --subnet-id "subnet-0999e0e982782fd01" \
+    --associate-public-ip-address \
+    --query "Instances[0].InstanceId"
+
+
